be6025f0562b95058c61ecf286fdefb337dfea0c
Sean Bennett <bennett.sean@gmail.com>
2012-03-10 16:40:09 -0600

    include lib folder contents

--- rakefile.rb
+++ rakefile.rb
@@ -4,6 +4,8 @@ require 'rake/clean'
 CLEAN.include("src/**/bin", "src/**/obj", "test/**/bin", "test/**/obj")
 CLOBBER.include('packages/**')
 
+sln_file = FileList['**/*.sln'].first
+
 desc "Download Dependencies"
 task :deps do
 	FileList["**/packages.config"].each { |filepath|
@@ -14,7 +16,7 @@ desc "Build Solution"
 msbuild :build do |msb|
   msb.properties = { :configuration => :Debug }
   msb.targets = [ :Clean, :Build ]
-  msb.solution = "#{FileList['**/*.sln'].first}" 
+  msb.solution = "#{sln_file}" 
 end
 
 desc "Run Unit Tests"
@@ -27,4 +29,19 @@ puts "#{mspec}"
 
 end
 
-task :default => ["deps","build","unit"]
+desc "All tests"
+task :tests => :unit do
+end
+
+desc "Publishes web application"
+msbuild :publish=>:tests do |m|
+    m.properties={:configuration=>:Release}
+    m.targets [:_WPPCopyWebApplication]
+    m.properties={
+         :webprojectoutputdire=>"c:/temp/outputdir/",
+	 :outdir=>"c:/temp/outputdir/bin/"
+	}
+    m.solution=sln_file
+end
+
+task :default => ["deps","build","tests"]
